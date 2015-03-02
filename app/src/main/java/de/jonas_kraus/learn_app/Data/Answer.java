08b6450e25ad0ff84e9d3024d10f2319df59be37
Jonas <jonaskraus@gmx.de>
2015-03-02 19:29:47 +0100

    Creating of Categories and cards on listview

--- app/src/main/java/de/jonas_kraus/learn_app/Data/Answer.java
+++ app/src/main/java/de/jonas_kraus/learn_app/Data/Answer.java
@@ -4,8 +4,34 @@ package de.jonas_kraus.learn_app.Data;
  * Created by Jonas on 01.03.2015.
  */
 public class Answer {
+    private int id;
     private boolean correct;
     private String answer;
+
+    public Answer(int id, boolean correct, String answer) {
+        this.id = id;
+        this.correct = correct;
+        this.answer = answer;
+    }
+    public Answer(boolean correct, String answer) {
+        this.correct = correct;
+        this.answer = answer;
+    }
+    public Answer(int id, String answer) {
+        this.id = id;
+        this.answer = answer;
+    }
+    public Answer(String answer) {
+        this.answer = answer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public boolean isCorrect() {
         return correct;
@@ -23,9 +49,4 @@ public class Answer {
         this.answer = answer;
     }
 
-    public Answer(boolean correct, String answer) {
-
-        this.correct = correct;
-        this.answer = answer;
-    }
 }
--- app/src/main/java/de/jonas_kraus/learn_app/activity/CatalogueHome.java
+++ app/src/main/java/de/jonas_kraus/learn_app/activity/CatalogueHome.java
@@ -2,6 +2,7 @@ package de.jonas_kraus.learn_app.activity;
 
 import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,12 +15,16 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.jonas_kraus.learn_app.Data.Answer;
 import de.jonas_kraus.learn_app.Data.Card;
 import de.jonas_kraus.learn_app.Data.Catalogue;
 import de.jonas_kraus.learn_app.Data.Category;
@@ -29,8 +34,11 @@ import de.jonas_kraus.learn_app.R;
 public class CatalogueHome extends ListActivity {
     private DbManager db;
     private ListView listViewCatalogue;
+    private TextView textViewPrompt;
+    private View promptView;
     private Button buttonAddCategory;
     private int currentCategoryParent = -1;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +59,8 @@ public class CatalogueHome extends ListActivity {
         setListAdapter(adapter);
         listViewCatalogue = getListView();
         buttonAddCategory = (Button) findViewById(R.id.buttonNewCategory);
+
+        context = this;
     }
 
     @Override
@@ -58,20 +68,79 @@ public class CatalogueHome extends ListActivity {
         super.onResume();
         openDb();
         addClickListenersToListView();
+        context = this;
     }
 
     private void addClickListenersToListView() {
         listViewCatalogue.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), "On item click", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(), "On item click", Toast.LENGTH_SHORT).show();
+
+                Catalogue curCat = (Catalogue) getListAdapter().getItem(position);
+                Log.d("View Item",curCat+"");
+                currentCategoryParent = curCat.getCategory().getId();
+
+                List<Category> categories = db.getCategoriesByLevel(currentCategoryParent);
+                List<Card> cards = db.getCardsByLevel(currentCategoryParent);
+                List<Catalogue> catalogue = new ArrayList<Catalogue>();
+                for (Category cat : categories) {
+                    catalogue.add(new Catalogue(cat));
+                }
+                for (Card card : cards) {
+                    catalogue.add(new Catalogue(card));
+                }
+                ArrayAdapter<Catalogue> adapter = new ArrayAdapter<Catalogue>(context, android.R.layout.simple_list_item_1, catalogue);
+                setListAdapter(adapter);
+
             }
         });
         listViewCatalogue.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), "On long click", Toast.LENGTH_SHORT).show();
-                return false;
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+
+                LayoutInflater layoutInflater = LayoutInflater.from(context);
+                promptView = layoutInflater.inflate(R.layout.prompt_add_card, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+                alertDialogBuilder.setView(promptView);
+
+                //final ArrayAdapter<Catalogue> adapter = (ArrayAdapter<Catalogue>) getListAdapter();
+                //adapter.add(new Catalogue(db.createCard(new Card());));
+
+                alertDialogBuilder.setCancelable(true).setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        final EditText inputQuestion = (EditText) promptView.findViewById(R.id.promptEditTextQuestion);
+                        final EditText inputAnswer = (EditText) promptView.findViewById(R.id.promptEditTextAnswer);
+                        final EditText inputHint = (EditText) promptView.findViewById(R.id.promptEditTextHint);
+                        final RadioGroup group = (RadioGroup) findViewById(R.id.promptRadioGroup);
+                        final RadioButton multiple = (RadioButton) findViewById(R.id.promptRadioButtonMultipleChoice);
+                        final RadioButton text = (RadioButton) findViewById(R.id.promptRadioButtonText);
+                        Card.CardType cardType = Card.CardType.MULTIPLECHOICE;
+                        List<Answer> answers = new ArrayList<Answer>();
+                        if (text == null || text.isChecked()) {
+                            cardType = Card.CardType.NOTECARD;
+                            answers.add(new Answer(inputAnswer.getText().toString()));
+                        } else {
+                            /* @TODO */
+                        }
+
+                        Catalogue curCat = (Catalogue) getListAdapter().getItem(position);
+                        Log.d("View Item",curCat+"");
+                        currentCategoryParent = curCat.getCategory().getId();
+                        Card card = db.createCard(new Card(cardType, inputQuestion.getText().toString(), answers, false, 0, inputHint.getText().toString(),currentCategoryParent));
+                        //adapter.add(new Catalogue(card));
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+
+                return true;
             }
         });
     }
@@ -81,11 +150,15 @@ public class CatalogueHome extends ListActivity {
         final ArrayAdapter<Catalogue> adapter = (ArrayAdapter<Catalogue>) getListAdapter();
         switch(view.getId()) {
             case R.id.buttonNewCategory:
+
                 LayoutInflater layoutInflater = LayoutInflater.from(this);
-                View promptView = layoutInflater.inflate(R.layout.prompt_add_category, null);
+                promptView = layoutInflater.inflate(R.layout.prompt_add_category, null);
+                textViewPrompt = (TextView) findViewById(R.id.textViewPrompt);
+
+                //textViewPrompt.setText("Enter category name:");
                 AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
                 alertDialogBuilder.setView(promptView);
-                final EditText input = (EditText) promptView.findViewById(R.id.promptAddCategoryInput);
+                final EditText input = (EditText) promptView.findViewById(R.id.promptAddCatalogueInput);
 
                 alertDialogBuilder.setCancelable(true).setPositiveButton("Save", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
