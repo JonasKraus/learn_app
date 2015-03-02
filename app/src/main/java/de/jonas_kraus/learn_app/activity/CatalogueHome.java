d49caa3a86fc145dc60d27b50555b21abeb8854e
Jonas <jonaskraus@gmx.de>
2015-03-02 11:34:17 +0100

    Added GUI and click listener

--- app/src/main/java/de/jonas_kraus/learn_app/activity/CatalogueHome.java
+++ app/src/main/java/de/jonas_kraus/learn_app/activity/CatalogueHome.java
@@ -1,20 +1,64 @@
 package de.jonas_kraus.learn_app.activity;
 
+import android.app.ListActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
+import java.sql.SQLException;
+
+import de.jonas_kraus.learn_app.Database.DbManager;
 import de.jonas_kraus.learn_app.R;
 
-public class CatalogueHome extends ActionBarActivity {
+public class CatalogueHome extends ListActivity {
+    private DbManager db;
+    private ListView listViewCatalogue;
+    private Button buttonAddCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalogue_home);
+
+        openDb();
+        listViewCatalogue = getListView();
+        buttonAddCategory = (Button) findViewById(R.id.buttonNewCategory);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        openDb();
+    }
+
+    public void onClick(View view) {
+        Log.d("CatalogueHome", "click: " + view.getId());
+        switch(view.getId()) {
+            case R.id.buttonNewCategory:
+        }
+    }
+
+    private void openDb() {
+        try {
+            if (db == null) {
+                db = new DbManager(this);
+            }
+            db.open();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        db.close();
+        buttonAddCategory.setOnClickListener(null);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
